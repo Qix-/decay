9a1193cdc01cb78cd497fef29c3ccdee57adcdc3
Josh Junon <Qix-@users.noreply.github.com>
2016-06-10 22:32:13 -0700

    Use simpler default parameter checking

--- decay.js
+++ decay.js
@@ -7,9 +7,8 @@
  * Corrected for decay errors in post
  */
 exports.redditHot = function (decay) {
-  if (decay == null) {
-    decay = 45000;
-  }
+  decay = decay || 45000;
+
   return function (ups, downs, date) {
     var s = ups - downs
       , order = Math.log(Math.max(Math.abs(s), 1)) / Math.LN10
@@ -23,9 +22,8 @@ exports.redditHot = function (decay) {
  * http://amix.dk/blog/post/19574
  */
 exports.hackerHot = function (gravity) {
-  if (gravity == null) {
-    gravity = 1.8;
-  }
+  gravity = gravity || 1.8;
+  
   return function (votes, itemDate) {
     var hourAge = (Date.now() - itemDate.getTime()) / (1000 * 3600);
     return (votes - 1) / Math.pow(hourAge + 2, gravity);
@@ -40,11 +38,9 @@ exports.hackerHot = function (gravity) {
  * http://www.evanmiller.org/how-not-to-sort-by-average-rating.html
  */
 exports.wilsonScore = function (z) {
-  if (z == null) {
-    // z represents the statistical confidence
-    // z = 1.0 => ~69%, 1.96 => ~95% (default)
-    z = 1.96;
-  }
+  // z represents the statistical confidence
+  // z = 1.0 => ~69%, 1.96 => ~95% (default)
+  z = z || 1.96;
 
   return function (ups, downs) {
     var n = ups + downs;
